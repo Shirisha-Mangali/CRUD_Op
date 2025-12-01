28ba0faffffda53ce413628d54402deb6ca60361
Shirisha <shirisha1862@gmail.com>
2025-12-01 09:07:43 +0530

    fst

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();// creates an Express application
+//this is server object
+//internally uses Node's http server
+
+const postsRouter=require('./routes/posts');
+app.use(express.json());
+
+app.use('/posts', postsRouter);
+
+app.listen(3000,()=>{
+    //res.send("hiii");
+    console.log("server runiing on http://localhost:3000");
+})
